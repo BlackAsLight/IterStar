2c74b62775a8687b015023741628d738b8728171
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2024-02-24 20:01:54 +1100

    Update mod.ts
    - Rewrote some types and functions and chose better variable names
    - Made The filter method debatably better for type narrowing?
    - Made AsyncIter able to take Iterables as well
    - Maybe more

--- mod.ts
+++ mod.ts
@@ -1,7 +1,5 @@
-type Flat<T> = T extends Iterable<infer U> ? Flat<U> : T
-type AsyncFlat<T> = T extends AsyncIterable<infer U> ? AsyncFlat<U> : T
-// deno-lint-ignore no-explicit-any
-type Force = any
+type Flat<T> = T extends Iterable<infer U> ? (U extends object ? Flat<U> : U) : T
+type AsyncFlat<T> = T extends AsyncIterable<infer U> ? (U extends object ? AsyncFlat<U> : U) : T
 
 export function* range(from = 0, to?: number): Generator<number> {
 	if (to == undefined) {
@@ -54,9 +52,9 @@ export async function parallel<T, U>(
 
 export class Iter<T> {
 	#gen: Generator<T>
-	constructor(array: { [Symbol.iterator](): Iterator<T> }) {
+	constructor(iterable: Iterable<T>) {
 		this.#gen = (function* () {
-			for (const x of array) yield x
+			for (const x of iterable) yield x
 		})()
 	}
 
@@ -80,43 +78,63 @@ export class Iter<T> {
 	}
 
 	map<U>(func: (x: T) => U): Iter<U> {
-		return new Iter(
-			(function* (iter) {
-				for (const x of iter) yield func(x)
-			})(this),
-		)
+		return new Iter<U>({
+			[Symbol.iterator]: () => ({
+				next: () => {
+					const next = this.#gen.next()
+					if (next.done) return { done: true, value: undefined }
+					return { done: false, value: func(next.value) }
+				},
+			}),
+		})
 	}
 
 	flat(): Iter<Flat<T>> {
-		return new Iter(
-			(function* (iter) {
-				for (const x of iter)
-					if ((x as Force)[Symbol.iterator]) for (const y of new Iter(x as Force).flat()) yield y
-					else yield x
-			})(this),
-		) as Force
+		return new Iter<Flat<T>>({
+			[Symbol.iterator]: () => ({
+				next: () => {
+					const iters: Iterator<T>[] = [this.#gen]
+					while (iters.length) {
+						const iter = iters.pop()!
+						const next = iter.next()
+						if (next.done) continue
+						iters.push(iter)
+						if (next.value && typeof next.value === 'object' && Symbol.iterator in next.value)
+							iters.push((next.value as Iterable<T>)[Symbol.iterator]())
+						else return { done: false, value: next.value as Flat<T> }
+					}
+					return { done: true, value: undefined }
+				},
+			}),
+		})
 	}
 
-	filter<U, V extends unknown = boolean>(func: (x: T) => V): Iter<Extract<T, U>> {
-		return new Iter(
-			(function* (iter) {
-				for (const x of iter) if (func(x)) yield x as Extract<T, U>
-			})(this),
-		)
+	filter<U extends T>(func: ((x: T) => x is U) | ((x: T) => unknown)): Iter<Extract<T, U>> {
+		return new Iter<Extract<T, U>>({
+			[Symbol.iterator]: () => ({
+				next: () => {
+					while (true) {
+						const next = this.#gen.next()
+						if (next.done) return { done: true, value: undefined }
+						if (func(next.value)) return { done: false, value: next.value as Extract<T, U> }
+					}
+				},
+			}),
+		})
 	}
 
 	slice(start = 0, end = Infinity): Iter<T> {
 		if (start < 0) start = 0
 		end -= start
 		return new Iter(
-			(function* (iter) {
+			(function* (gen) {
 				if (end-- > 0)
-					for (const x of iter) {
+					for (const x of gen) {
 						if (start-- > 0) continue
 						yield x
 						if (end-- <= 0) break
 					}
-			})(this),
+			})(this.#gen),
 		)
 	}
 
@@ -127,44 +145,42 @@ export class Iter<T> {
 			init = this.shift()
 			if (init == undefined) throw Error('Uncaught TypeError: reduce of empty iter with no initial value')
 		}
-		for (const x of this.#gen) {
-			init = func(init, x)
-		}
+		for (const x of this.#gen) init = func(init, x)
 		return init
 	}
 
 	join(sep = ''): string {
-		let output = ''
-		for (const x of this.#gen) {
-			output += sep + x
-		}
-		return output.slice(sep.length)
-	}
-
-	[Symbol.iterator](): { next: () => IteratorResult<T> } {
-		return {
-			next: () => this.#gen.next(),
-		}
-	}
-
-	get readable(): ReadableStream<T> {
-		return ReadableStream.from(this.#gen)
+		let output = '' + ((this.#gen.next().value as T | undefined) ?? '')
+		for (const x of this.#gen) output += sep + x
+		return output
 	}
 
 	toAsync(): AsyncIter<T> {
-		return new AsyncIter(
-			(async function* (gen) {
-				for (const x of gen) yield x
-			})(this.#gen),
-		)
+		return new AsyncIter(this)
+	}
+
+	get readable(): ReadableStream<T> {
+		return new ReadableStream({
+			pull: controller => {
+				const next = this.#gen.next()
+				if (next.done) controller.close()
+				else controller.enqueue(next.value)
+			},
+		})
+	}
+
+	[Symbol.iterator](): Iterator<T> {
+		return {
+			next: () => this.#gen.next(),
+		}
 	}
 }
 
 export class AsyncIter<T> {
 	#gen: AsyncGenerator<T>
-	constructor(array: { [Symbol.asyncIterator](): AsyncIterator<T> }) {
+	constructor(iterable: Iterable<T> | AsyncIterable<T>) {
 		this.#gen = (async function* () {
-			for await (const x of array) yield x
+			for await (const x of iterable) yield x
 		})()
 	}
 
@@ -184,47 +200,67 @@ export class AsyncIter<T> {
 	}
 
 	async forEach<U>(func: (x: T) => U): Promise<void> {
-		for await (const x of this.#gen) func(x)
+		for await (const x of this.#gen) await func(x)
 	}
 
 	map<U>(func: (x: T) => U): AsyncIter<U> {
-		return new AsyncIter(
-			(async function* (asyncIter) {
-				for await (const x of asyncIter) yield func(x)
-			})(this),
-		)
+		return new AsyncIter<U>({
+			[Symbol.asyncIterator]: () => ({
+				next: async () => {
+					const next = await this.#gen.next()
+					if (next.done) return { done: true, value: undefined }
+					return { done: false, value: await func(next.value) }
+				},
+			}),
+		})
 	}
 
 	flat(): AsyncIter<AsyncFlat<T>> {
-		return new AsyncIter(
-			(async function* (asyncIter) {
-				for await (const x of asyncIter)
-					if ((x as Force)[Symbol.asyncIterator]) for await (const y of new AsyncIter(x as Force).flat()) yield y
-					else yield x
-			})(this),
-		) as Force
+		return new AsyncIter<AsyncFlat<T>>({
+			[Symbol.asyncIterator]: () => ({
+				next: async () => {
+					const iters: AsyncIterator<T>[] = [this.#gen]
+					while (iters.length) {
+						const iter = iters.pop()!
+						const next = await iter.next()
+						if (next.done) continue
+						iters.push(iter)
+						if (next.value && typeof next.value === 'object' && Symbol.asyncIterator in next.value)
+							iters.push((next.value as AsyncIterable<T>)[Symbol.asyncIterator]())
+						else return { done: false, value: next.value as AsyncFlat<T> }
+					}
+					return { done: true, value: undefined }
+				},
+			}),
+		})
 	}
 
-	filter<U, V extends unknown = boolean>(func: (x: T) => V): AsyncIter<Extract<T, U>> {
-		return new AsyncIter(
-			(async function* (asyncIter) {
-				for await (const x of asyncIter) if (func(x)) yield x as Extract<T, U>
-			})(this),
-		)
+	filter<U extends T>(func: ((x: T) => x is U) | ((x: T) => unknown)): AsyncIter<Extract<T, U>> {
+		return new AsyncIter<Extract<T, U>>({
+			[Symbol.asyncIterator]: () => ({
+				next: async () => {
+					while (true) {
+						const next = await this.#gen.next()
+						if (next.done) return { done: true, value: undefined }
+						if (await func(next.value)) return { done: false, value: next.value as Extract<T, U> }
+					}
+				},
+			}),
+		})
 	}
 
 	slice(start = 0, end = Infinity): AsyncIter<T> {
 		if (start < 0) start = 0
 		end -= start
 		return new AsyncIter(
-			(async function* (asyncIter) {
+			(async function* (gen) {
 				if (end-- > 0)
-					for await (const x of asyncIter) {
+					for await (const x of gen) {
 						if (start-- > 0) continue
 						yield x
 						if (end-- <= 0) break
 					}
-			})(this),
+			})(this.#gen),
 		)
 	}
 
@@ -235,28 +271,30 @@ export class AsyncIter<T> {
 			init = await this.shift()
 			if (init == undefined) throw Error('Uncaught TypeError: reduce of empty iter with no initial value')
 		}
-		for await (const x of this.#gen) {
-			init = await func(init, x)
-		}
+		for await (const x of this.#gen) init = await func(init, x)
 		return init
 	}
 
 	async join(sep = ''): Promise<string> {
-		let output = ''
-		for await (const x of this.#gen) {
-			output += sep + x
-		}
+		let output = '' + (((await this.#gen.next()).value as T | undefined) ?? '')
+		for await (const x of this.#gen) output += sep + x
 		return output.slice(sep.length)
 	}
 
-	[Symbol.asyncIterator](): { next: () => Promise<IteratorResult<T>> } {
+	get readable(): ReadableStream<T> {
+		return new ReadableStream({
+			pull: async controller => {
+				const next = await this.#gen.next()
+				if (next.done) controller.close()
+				else controller.enqueue(next.value)
+			},
+		})
+	}
+
+	[Symbol.asyncIterator](): AsyncIterator<T> {
 		return {
 			next: () => this.#gen.next(),
 		}
-	}
-
-	get readable(): ReadableStream<T> {
-		return ReadableStream.from(this.#gen)
 	}
 }
 
@@ -264,8 +302,8 @@ export class Queue<T> {
 	#head = 0
 	#tail = 0
 	#list: { [k: string]: T } = {}
-	constructor(arrayLike?: { [Symbol.iterator](): Iterator<T> }) {
-		if (arrayLike) for (const value of arrayLike) this.#list[this.#tail++] = value
+	constructor(iterable?: Iterable<T>) {
+		if (iterable) for (const x of iterable) this.#list[this.#tail++] = x
 	}
 
 	get length(): number {
@@ -280,9 +318,9 @@ export class Queue<T> {
 
 	shift(): T | undefined {
 		if (this.#head < this.#tail) {
-			const value = this.#list[this.#head]
+			const x = this.#list[this.#head]
 			delete this.#list[this.#head++]
-			return value
+			return x
 		}
 	}
 
@@ -293,14 +331,14 @@ export class Queue<T> {
 
 	pop(): T | undefined {
 		if (this.#head < this.#tail) {
-			const value = this.#list[--this.#tail]
+			const x = this.#list[--this.#tail]
 			delete this.#list[this.#tail]
-			return value
+			return x
 		}
 	}
 
 	push(...array: T[]): number {
-		for (const value of array) this.#list[this.#tail++] = value
+		for (const x of array) this.#list[this.#tail++] = x
 		return this.length
 	}
 
@@ -308,7 +346,7 @@ export class Queue<T> {
 		return this.#list[i + this.#head]
 	}
 
-	[Symbol.iterator](): { next: () => IteratorResult<T> } {
+	[Symbol.iterator](): Iterator<T> {
 		return {
 			next: () => ({
 				done: !this.length,
